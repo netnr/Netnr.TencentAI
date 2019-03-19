8e6c73ff8a8cad6496179f4d6fd15068d94e5a70
netnr <netnr@netnr.com>
2019-03-19 16:46:54 +0800

    update

--- src/Netnr.TencentAI/Netnr.TencentAI/HttpTo.cs
+++ src/Netnr.TencentAI/Netnr.TencentAI/HttpTo.cs
@@ -9,16 +9,15 @@ namespace Netnr.TencentAI
     /// </summary>
     public class HttpTo
     {
-        #region 服务器 发送请求
         /// <summary>
-        /// 发送请求参数设置
+        /// HttpWebRequest对象
         /// </summary>
         /// <param name="url">地址</param>
-        /// <param name="type">请求类型 默认GET </param>
-        /// <param name="postData">POST发送内容 默认空</param>
-        /// <param name="charset">编码</param>
+        /// <param name="type">请求类型，默认GET</param>
+        /// <param name="data">POST数据</param>
+        /// <param name="charset">编码，默认utf-8</param>
         /// <returns></returns>
-        public static HttpWebRequest HWRequest(string url, string type = "GET", string postData = "", string charset = "utf-8")
+        public static HttpWebRequest HWRequest(string url, string type = "GET", string data = null, string charset = "utf-8")
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = type;
@@ -28,11 +27,11 @@ namespace Netnr.TencentAI
             request.Timeout = short.MaxValue * 3;//MS
             request.ContentType = "application/x-www-form-urlencoded";
 
-            if (type != "GET")
+            if (type != "GET" && data != null)
             {
                 //发送内容
-                byte[] bytes = Encoding.GetEncoding(charset).GetBytes(postData);
-                request.ContentLength = Encoding.GetEncoding(charset).GetBytes(postData).Length;
+                byte[] bytes = Encoding.GetEncoding(charset).GetBytes(data);
+                request.ContentLength = Encoding.GetEncoding(charset).GetBytes(data).Length;
                 Stream outputStream = request.GetRequestStream();
                 outputStream.Write(bytes, 0, bytes.Length);
                 outputStream.Close();
@@ -41,62 +40,49 @@ namespace Netnr.TencentAI
         }
 
         /// <summary>
-        /// 发送请求 得到请求结果
+        /// HTTP请求
         /// </summary>
-        /// <param name="request">HttpWebRequest对象 可通过HWRequest方法创建</param>
-        /// <param name="e">返回类容编码 默认UTF-8</param>
-        /// <returns></returns>
-        public static string Url(HttpWebRequest request, Encoding e)
-        {
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            Stream responseStream = response.GetResponseStream();
-            if (string.Compare(response.ContentEncoding, "gzip", true) >= 0)
-                responseStream = new System.IO.Compression.GZipStream(responseStream, System.IO.Compression.CompressionMode.Decompress);
-            StreamReader reader = new StreamReader(responseStream, e);
-            string result = "", strData = "";
-            //result = reader.ReadToEnd();
-            while ((strData = reader.ReadLine()) != null)
-            {
-                result += strData + "\r\n";
-            }
-            reader.Close();
-            responseStream.Close();
-            return result;
-        }
-
-        /// <summary>
-        /// 发送请求 得到请求结果
-        /// </summary>
-        /// <param name="request">HttpWebRequest对象 可通过HWRequest方法创建</param>
-        /// <param name="charset">编码</param>
+        /// <param name="request">HttpWebRequest对象</param>
+        /// <param name="charset">编码，默认utf-8</param>
         /// <returns></returns>
         public static string Url(HttpWebRequest request, string charset = "utf-8")
         {
-            return Url(request, Encoding.GetEncoding(charset));
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+            if (string.Compare(response.ContentEncoding, "gzip", true) >= 0)
+                responseStream = new System.IO.Compression.GZipStream(responseStream, System.IO.Compression.CompressionMode.Decompress);
+
+            using (var sr = new StreamReader(responseStream, Encoding.GetEncoding(charset)))
+            {
+                var result = sr.ReadToEnd();
+                return result;
+            }
         }
 
         /// <summary>
-        /// 发送 GET 请求 得到请求结果
+        /// GET请求
         /// </summary>
-        /// <param name="url">URL</param>
-        /// <param name="charset">编码</param>
+        /// <param name="url">地址</param>
+        /// <param name="charset">编码，默认utf-8</param>
         /// <returns></returns>
         public static string Get(string url, string charset = "utf-8")
         {
-            return Url(HWRequest(url, "GET", null, charset), Encoding.GetEncoding(charset));
+            var request = HWRequest(url, "GET", null, charset);
+            return Url(request, charset);
         }
 
         /// <summary>
-        /// 发送 POST 请求
+        /// POST请求
         /// </summary>
-        /// <param name="url">URL</param>
-        /// <param name="postData">发送内容</param>
-        /// <param name="charset">编码</param>
+        /// <param name="url">地址</param>
+        /// <param name="data">POST数据</param>
+        /// <param name="charset">编码，默认utf-8</param>
         /// <returns></returns>
-        public static string Post(string url, string postData, string charset = "utf-8")
+        public static string Post(string url, string data, string charset = "utf-8")
         {
-            return Url(HWRequest(url, "POST", postData, charset));
+            var request = HWRequest(url, "POST", data, charset);
+            return Url(request, charset);
         }
-        #endregion
     }
 }
